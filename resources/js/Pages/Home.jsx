8156fd6773f81dca0e1215b9b4e340854c81da88
Zaw Min Hein <zawminheinmp@gmail.com>
2025-10-10 14:22:31 +0630

    Inertia + Tailwind

---
+++ resources/js/Pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <div className='text-3xl text-orange-500'>Home</div>
+  )
+}
+
+export default Home
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
-Route::get('/', function () {
-    return view('welcome');
+// Route::get('/', function () {
+//     return Inertia::render('Home');
+// });
+
+Route::inertia('/','Home');
+
+Route::get('/home', function(){
+    return Inertia::render('Home');
 });
